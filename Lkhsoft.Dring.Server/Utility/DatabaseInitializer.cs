38f30cbbc09519557938158dd6366047cb581f1f
nakira974 <maxime.loukhal@gmail.com>
2025-02-24 00:48:45 +0100

    Fixed Docker config

--- Lkhsoft.Dring.Server/Utility/DatabaseInitializer.cs
+++ Lkhsoft.Dring.Server/Utility/DatabaseInitializer.cs
@@ -2,6 +2,8 @@
 
 using System.Configuration;
 using System.Data.SQLite;
+using Lkhsoft.Dring.Server.Cli.Commands.Authentication;
+using Lkhsoft.Dring.Shared.Core;
 
 #endregion
 
@@ -36,5 +38,57 @@ public class DatabaseInitializer
 
         await using var command = new SQLiteCommand(sqlScript, connection);
         await command.ExecuteNonQueryAsync();
+        await InsertGuestIfNoExistsAsync();
+    }
+    
+    /// <summary>
+    /// Create a guest in the database if not exists
+    /// </summary>
+    /// <param name="username">Username of the new user</param>
+    /// <param name="encryptedPassword">Encrypted password</param>
+    /// <param name="iv">Initialization vector</param>
+    /// <returns>Une tâche asynchrone.</returns>
+    private async Task InsertGuestIfNoExistsAsync()
+    {
+        const string existsQuery = @"
+            SELECT 1 
+            FROM Users
+            WHERE Username = 'guest'
+            LIMIT 1;
+        ";
+
+        await using var connection = new SQLiteConnection(_connectionString);
+        await connection.OpenAsync();
+        await using var command1 = new SQLiteCommand(existsQuery, connection);
+        var result = await command1.ExecuteScalarAsync();
+        var exists = result is not null;
+
+        if (!exists)
+        {
+            const string query = @"
+            INSERT INTO Users (Username, Password, IsConnected, IV)
+            VALUES ('guest', '', 0, 'none');
+        ";
+
+            await connection.OpenAsync();
+
+            await using var transaction = connection.BeginTransaction();
+            await using var command = new SQLiteCommand(query, connection, transaction);
+            try
+            {
+                await command.ExecuteNonQueryAsync();
+
+                await transaction.CommitAsync();
+            }
+            catch
+            {
+                await transaction.RollbackAsync();
+                throw;
+            }
+        }
+        else
+        {
+            Console.WriteLine("Guest user already exists");
+        }
     }
 }
